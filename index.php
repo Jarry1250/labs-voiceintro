48501a10b8afadcb989e926ef3eabcd8bebf188b
jarry1250 <jarry1250@gmail.com>
2014-01-19 17:07:25 +0000

    Take HTML snippets out of PHP tags where possible (index.php)

--- index.php
+++ index.php
@@ -35,35 +35,41 @@
 
 	// $username is fakeable using a MITM under OAuth Connect goes live
 	$username = $oAuth->authorizeMe();
+?>
+	<div class="mw-voiceintro-message">Hello <?=$username ?></div>
+		<div class="mw-voiceintro-toolbar">
+			<button class="mw-voiceintro-record" disabled ><?= _html( 'voiceintro-toolbar-record-label', 'voiceintro' ) ?></button>
+			<button class="mw-voiceintro-stop" disabled ><?= _html( 'voiceintro-toolbar-stop-label', 'voiceintro' ) ?></button>
+			<button class="mw-voiceintro-clear" disabled ><?= _html( 'voiceintro-toolbar-clear-label', 'voiceintro' ) ?></button>
+		<div class="mw-voiceintro-preview-div" ></div>
+	</div>
+	<form>
+		<fieldset>
+			<legend><?= _html( 'voiceintro-information-label', 'voiceintro' ) ?></legend>
+			<b><?= _html( 'voiceintro-information-speaker-label', 'voiceintro' ) ?></b><input type="text" class="mw-voiceintro-information-speaker"><br><br>
+			<b><?= _html( 'voiceintro-information-language-label', 'voiceintro' ) ?></b><select class="mw-voiceintro-information-language">
+				<?php
+					$languages = $I18N->getLangNames();//array( 'en' => 'English' );
+					foreach( $languages as $lang_code => $lang_name) {
+						echo '<option value="' . $lang_code . '">' . $lang_code.' - '.$lang_name  . '</option>';
+					}
+				?>
+			</select>
+			<br /><br />
+			<div class="mw-voiceintro-information-license-cc-by-sa" >
+				<?php
+					//.  _html( 'voiceintro-information-license-prompt:$user', 'voiceintro' );
+					echo 'message:mwe-upwiz-source-ownwork-assert-cc-by-sa-3.0:1:$user:http://creativecommons.org/licenses/by-sa/3.0/deed.en' . '<br>';
+					//_html( 'mwe-upwiz-source-ownwork-cc-by-sa-3.0-explain:1', 'voiceintro' )
+				?>
+				<br/>
+			</div>';
+		</fieldset>
+	</form>
 
-	echo "Hello $username.";
-
-	echo '<div class="mw-voiceintro-message" ></div>';
-	echo '<div class="mw-voiceintro-toolbar" >';
-	echo '<button class="mw-voiceintro-record" disabled >' . _html( 'voiceintro-toolbar-record-label', 'voiceintro' ).'</button>';
-	echo '<button class="mw-voiceintro-stop" disabled >' . _html( 'voiceintro-toolbar-stop-label', 'voiceintro' ).'</button>';
-	echo '<button class="mw-voiceintro-clear" disabled >' . _html( 'voiceintro-toolbar-clear-label', 'voiceintro' ).'</button>';
-	echo '<div class="mw-voiceintro-preview-div" ></div>';
-	echo '</div>';
-	echo '<form>';
-	echo '<fieldset>';
-	echo '<legend>'. _html( 'voiceintro-information-label', 'voiceintro' ).'</legend>';
-	echo '<b>'. _html( 'voiceintro-information-speaker-label', 'voiceintro' ).'</b><input type="text" class="mw-voiceintro-information-speaker"><br><br>';
-	$languages = array( 'en' => 'English' ); //Language::getLanguageNames();
-	echo '<b>'. _html( 'voiceintro-information-language-label', 'voiceintro' ).'</b><select class="mw-voiceintro-information-language">';
-	foreach( $languages as $lang_code => $lang_name) {
-		echo '<option value="' . $lang_code . '">' . $lang_code.' - '.$lang_name  . '</option>';
-	}
-	echo '</select>';
-	echo '<br><br>';//.  _html( 'voiceintro-information-license-prompt:$user', 'voiceintro' );
-	echo '<div class="mw-voiceintro-information-license-cc-by-sa" >'.
-	 'message:mwe-upwiz-source-ownwork-assert-cc-by-sa-3.0:1:$user:http://creativecommons.org/licenses/by-sa/3.0/deed.en' . '<br>';
-	echo  _html( 'mwe-upwiz-source-ownwork-cc-by-sa-3.0-explain:1', 'voiceintro' ) . '<br></div>';
-	echo '</fieldset>';
-	echo '</form>';
-
-	echo '<script type="text/javascript" src="resources/jquery.fileDetails.js"></script>';
-	echo '<script type="text/javascript" src="resources/jquery.pronunciationRecorder.js"></script>';
-	echo '<script type="text/javascript" src="resources/jquery.general.js"></script>';
-	echo '<script type="text/javascript" src="resources/recorder.js"></script>';
+	<script type="text/javascript" src="resources/jquery.fileDetails.js"></script>
+	<script type="text/javascript" src="resources/jquery.pronunciationRecorder.js"></script>
+	<script type="text/javascript" src="resources/jquery.general.js"></script>
+	<script type="text/javascript" src="resources/recorder.js"></script>
+<?php
 	echo get_html( 'footer' );
